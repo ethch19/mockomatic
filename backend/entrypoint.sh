295889b6225ecc8ed8f5e6e10a88aa058bfa9d27
ethch19 <53605846+ethch19@users.noreply.github.com>
2026-01-19 14:27:10 +0000

    fix: invalid docker dev env 'expose' ports

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+echo "Waiting for PostgreSQL to start..."
+until sqlx database setup; do
+  echo "Postgres is unavailable - sleeping"
+  sleep 2
+done
+
+echo "Postgres is up - executing migrations"
 sqlx migrate run
 
 exec "$@"
